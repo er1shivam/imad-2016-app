64e568b7d4de2565c99cd6790ff2d0014e754b9d
Shivam srivastava <er1shivam@gmail.com>
2016-10-31 00:27:10 +0530

    server file

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/abc.php', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'abc.php'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
