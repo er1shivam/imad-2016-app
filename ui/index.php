26f060d3718abe1664755a93f8044f5c3f2eea3c
Shivam srivastava <er1shivam@gmail.com>
2016-10-31 00:21:09 +0530

    check php

---
+++ ui/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        This is just to check whther php is supported or not
+    </p>
+
+
+
+    <?php echo 2+3;
+    ?>
+    <br/>
+
+    <?php echo "hello world";
+    ?>
+
+
+
+
+
+</body>
+</html>
